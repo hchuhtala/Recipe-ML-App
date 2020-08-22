5468c4602b9d6cb41e6ce8b70467bdf7bae3f23d
Ian Mac Moore <ianmac@blueplusred.com>
2020-08-22 00:46:05 -0500

    Added env for Spoonacular API and call now uses it, upped recipe results to 6

--- recrec_app/static/js/config.js
+++ recrec_app/static/js/config.js
@@ -1,2 +1,7 @@
 //MapBox api key
 const API_KEY = "pk.eyJ1IjoiemVuZmluaXR5IiwiYSI6ImNrYzN1NXc2NDAxZ2QycGswbDRsZTlsdHMifQ.3zPvXmdrxcbe2RjdqrXoQQ";
+
+// const API_KEY_S = fetch("/key").then(function (response) {
+//     return response.text();
+// });
+// console.log("env? ", API_KEY_S);
